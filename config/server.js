1b4243558e742e6fbb4976c4066ad42676279f8a
dev-launchers-flux <66158749+dev-launchers-flux@users.noreply.github.com>
2021-06-01 21:57:15 +0100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '89f3d0d1ff175b5cb4e6e6e642697183'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c2ff1d92-c770-4ddd-b8c7-d145b7d2980f'
+};
