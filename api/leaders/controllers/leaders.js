5b5913061f0b9ebad98631f953ef9212de2f447e
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-06-08 10:25:48 +0300

    feat(Enjoy2Live): created Projects & Leaders collection

--- api/leaders/controllers/leaders.js
+++ api/leaders/controllers/leaders.js
@@ -1,8 +1,28 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.leaders.search(ctx.query);
+    } else {
+      entities = await strapi.services.leaders.find(ctx.query);
+    }
+
+    return entities.map((entity) => entity.user);
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.leaders.findOne({ id });
+    return Object.fromEntries(
+      Object.entries(entity).filter(([k, v]) => k == "user" || k == "project")
+    );
+  },
+};
