87bf1f9d92fd518b529b31a02d7209671e18767b
alearm <alearm246@gmail.com>
2021-06-09 02:45:33 -0500

    feat(alearm246): made it so points can be fetched via user id

--- api/point/controllers/point.js
+++ api/point/controllers/point.js
@@ -1,8 +1,19 @@
 'use strict';
 
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+//this allows us to get points via user id
+
+module.exports = {
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.point.findOne({ user: id })
+    return sanitizeEntity(entity, { model: strapi.models.point });
+  }
+};
