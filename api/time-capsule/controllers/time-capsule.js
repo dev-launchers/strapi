a46a79ee65bcacc87a1f7774c8d4f168f23e12d8
jiawen <70170238+jiawen195@users.noreply.github.com>
2021-06-29 22:39:05 -0700

    fix(jiawen): link time-capsule to the user

--- api/time-capsule/controllers/time-capsule.js
+++ api/time-capsule/controllers/time-capsule.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { sanitizeEntity } = require('strapi-utils');
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
@@ -17,6 +17,34 @@ module.exports = {
     }
     const entity = await strapi.services["time-capsule"].find(params)
     return sanitizeEntity(entity, { model: strapi.models["time-capsule"] });
+  },
+
+  async create(ctx) {
+    const { id } = ctx.state.user;
+    const {
+      generalDayInformation,
+      acquiredKnowledge,
+      sessionRating,
+      gaveHelp,
+      volunteerHours,
+      receivedHelp,
+      pairUpQuestion,
+      extraInformation
+    } = ctx.request.body;
+    const timeCapsule = {
+      generalDayInformation,
+      acquiredKnowledge,
+      sessionRating,
+      gaveHelp,
+      volunteerHours,
+      receivedHelp,
+      pairUpQuestion,
+      extraInformation,
+      user: id
+    };
+    const entity = await strapi.services["time-capsule"].create(timeCapsule);
+    return sanitizeEntity(entity, { model: strapi.models["time-capsule"] });
   }
+
 
 };
