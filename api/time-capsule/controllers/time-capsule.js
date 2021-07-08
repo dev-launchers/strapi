d94f82f7255b893957c4a8f0d19af4c1a31a1ec5
jiawen <70170238+jiawen195@users.noreply.github.com>
2021-07-08 15:19:21 -0700

    fix(jiawen): update volunteerHours to point

--- api/time-capsule/controllers/time-capsule.js
+++ api/time-capsule/controllers/time-capsule.js
@@ -17,6 +17,39 @@ module.exports = {
     };
     const entity = await strapi.services['time-capsule'].find(params);
     return sanitizeEntity(entity, { model: strapi.models['time-capsule'] });
+  },
+
+  async create(ctx) {
+    const { id } = ctx.state.user;
+    const {
+      generalDayInformation,
+      acquiredKnowledge,
+      sessionRating,
+      gaveHelp,
+      volunteerHours,
+      receivedHelp,
+      pairUpQuestion,
+      extraInformation
+    } = ctx.request.body;
+
+    const timeCapsule = {
+      generalDayInformation,
+      acquiredKnowledge,
+      sessionRating,
+      gaveHelp,
+      volunteerHours,
+      receivedHelp,
+      pairUpQuestion,
+      extraInformation,
+      user: id
+    };
+
+    const entity = await strapi.services['time-capsule'].create(timeCapsule);
+    // update volunteerHours to point
+    const point = await strapi.services.point.findOne({ user: id });
+    const newVolunteerHours = volunteerHours + point.volunteerHours;
+    const updatePoint = await strapi.services.point.update({ user: id }, { volunteerHours: newVolunteerHours });
+    return sanitizeEntity(entity, { model: strapi.models['time-capsule'] });
   }
 
 };
