7e517a1ba708205fd4abf97e2d75bf43a5259751
alearm <alearm246@gmail.com>
2021-07-11 02:18:22 -0500

    fix(alearm246): replaced double quotes with single quotes

--- api/custom/controllers/custom.js
+++ api/custom/controllers/custom.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async logout(ctx) {
-    console.log("Successfully logged out");
+    console.log('Successfully logged out');
     ctx.cookies.set('token', null);
     ctx.send({
       message: 'Successfully logged out',
