9b72906a0d71a7bde30fa8f3ee259998056b2a0e
alearm <alearm246@gmail.com>
2021-07-13 00:42:58 -0500

    fix(alearm246): passed in options object when clearing cookie

--- api/custom/controllers/custom.js
+++ api/custom/controllers/custom.js
@@ -1,15 +1,25 @@
 'use strict';
+
+const { isDevEnv } = require("../../../utils/isDevEnv");
+
 module.exports = {
   async logout(ctx) {
     console.log('Successfully logged out');
-    const token = ctx.cookies.get('token');
-    if(!token) {
-      console.log('TOKEN IS NULL: ', token);
+    if (isDevEnv()) {
+      ctx.cookies.set('token', null, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age,
+        domain: 'localhost'
+      });
     } else {
-      console.log('TOKEN IN LOGOUT CONTROLLER: ', token);
+      ctx.cookies.set('token', null, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age,
+        domain: 'devlaunchers.com'
+      });
     }
 
-    ctx.cookies.set('token', null);
     ctx.send({
       message: 'Successfully logged out',
     });
