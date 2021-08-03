59e378afe878901fa092264db3fe55668627defd
chungthuang <7826979+chungthuang@users.noreply.github.com>
2021-08-03 23:03:36 +0100

    fix(chungthuang): Fix logger level

--- config/middleware.js
+++ config/middleware.js
@@ -6,7 +6,7 @@ module.exports = {
       origin: [process.env.FRONTEND_URL, process.env.URL, 'http://localhost:3000'],
     },
     logger: {
-      level: true, // process.env.STRAPI_LOG_LEVEL,
+      level: 'debug', // process.env.STRAPI_LOG_LEVEL,
       exposeInContext: true,
       // Allow requests log in dev env
       requests: true, // process.env.NODE_ENV == 'development'
