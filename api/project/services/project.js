66b63019a517b8e1c6ad98a431dd779634354418
alearm246 <47065061+alearm246@users.noreply.github.com>
2021-08-05 15:54:56 -0500

    fix(alearm246): remove unecessary comments

--- api/project/services/project.js
+++ api/project/services/project.js
@@ -4,15 +4,5 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
-/*
- const calendarScopes = ['https://www.googleapis.com/auth/calendar'];
-
- const client = new google.auth.GoogleAuth({
-   keyFile: "/srv/app/google-directory-key.json",
-   scopes: calendarScopes
- })
-
- const calendar = google.calendar({ version: '3', auth: client });*/
-
 
 module.exports = {};
