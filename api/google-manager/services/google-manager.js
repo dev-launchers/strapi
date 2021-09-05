fccaa84ef43717bb80c84f42958aa476b0118cb2
alearm246 <47065061+alearm246@users.noreply.github.com>
2021-09-05 11:38:45 -0500

    fix(alearm246): add comment specifying what transparent does

--- api/google-manager/services/google-manager.js
+++ api/google-manager/services/google-manager.js
@@ -201,6 +201,7 @@ class GoogleManager {
             }
           },
           summary: `${title} Common Room`,
+          //This prevents the event from blocking time on the calendar
           transparency: 'transparent'
         }
       });
