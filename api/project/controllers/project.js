562be4976a82a862c87a038f96f5c452f856adee
alearm246 <47065061+alearm246@users.noreply.github.com>
2021-10-18 00:22:59 -0500

    fix(alearm246): revert code

--- api/project/controllers/project.js
+++ api/project/controllers/project.js
@@ -84,15 +84,15 @@ module.exports = {
     try {
       const { interests } = ctx.state.user;
       const projects = await strapi.services.project.find();
-      const recommendedProjectsSet = new Set();
+      const recommendedProjectSlugs = [];
+      const recommendedProjects = [];
 
-      const addProjectsToSet = (projects, userCategory) => {
+      const getProjectSlugs = (projects, userCategory) => {
         for(const project of projects){
           for(const projectInterest of project.interests){
             for(const projectCategories of projectInterest.categories){
-              if((projectCategories.category === userCategory.category)){
-                const projectJSON = JSON.stringify(project);
-                recommendedProjectsSet.add(projectJSON);
+              if((projectCategories.category === userCategory.category) && (!(recommendedProjectSlugs.includes(project.slug)))){
+                recommendedProjectSlugs.push(project.slug);
               }
             }
           }
@@ -106,12 +106,18 @@ module.exports = {
       */
       for(const interest of interests){
         for(const category of interest.categories){
-          addProjectsToSet(projects, category);
+          getProjectSlugs(projects, category);
         }
       }
 
-      const recommendedProjectsJSON = [...recommendedProjectsSet];
-      const recommendedProjects = recommendedProjectsJSON.map(recommendedProjectJSON => JSON.parse(recommendedProjectJSON));
+      /*
+        Using the project slugs, I get the project and add them to
+        the recommendedProjects array
+      */
+      for(const recommendedProjectSlug of recommendedProjectSlugs){
+        const recommendedProject = await strapi.services.project.findOne({slug: recommendedProjectSlug});
+        recommendedProjects.push(recommendedProject);
+      }
 
       return ctx.send(recommendedProjects);
     } catch(err) {
