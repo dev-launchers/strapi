253270dd71921b9f485114fa971da05c11808309
kuikuigaoayata <kuikui.gao@ayata.com>
2021-10-23 15:00:32 -0500

    fix: modify applicant.js to notify the project leader when a new applicant joined the project

--- api/applicant/controllers/applicant.js
+++ api/applicant/controllers/applicant.js
@@ -26,6 +26,7 @@ module.exports = {
       level
     } = ctx.request.body;
 
+    console.log('new application');
 
     const applicantProject = await strapi.services.project.findOne({ slug: project });
 
@@ -41,12 +42,33 @@ module.exports = {
       commitment,
       accepted,
       reason,
-      project: applicantProject.id,
+      project: applicantProject ? applicantProject : null,
       level
     };
 
     const entity = await strapi.services.applicant.create(application);
+    console.log(entity);
 
+    if (applicantProject){
+      const team = applicantProject.team;
+      console.log(team);
+      if(team.leaders){
+        //lets leaders join google group
+        team.leaders.forEach(async (leader) => {
+          try {
+            const id = leader.leader.id ? leader.leader.id : leader.leader;
+            const email = leader.leader.email ? leader.leader.email: null;
+            console.log(email)
+            // Send an email to project lead.
+            await strapi.services.sendmail.send('kuikuigaocn@gmail.com', email, 'New applicant notificaton', `Hi Leaders, ${application.name} joined the project of "${applicantProject.title}".`);
+            
+          } catch(err) {
+            console.error('error: can not notify the project leader.', err);
+          }
+        });
+      };
+    }
+    
     return sanitizeEntity(entity, { model: strapi.models.applicant});
-  }
+  },
 };
---
+++ api/sendmail/services/sendmail.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+
+// Create reusable transporter object using SMTP transport.
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'kuikuigaocn@gmail.com',
+    pass: '123gaokuikui',
+  },
+});
+
+module.exports = {
+  send: (from, to, subject, text) => {
+    // Setup e-mail data.
+    const options = {
+      from,
+      to,
+      subject,
+      text,
+    };
+
+    // Return a promise of the function that sends the email.
+    return transporter.sendMail(options);
+  },
+};
