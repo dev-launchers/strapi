b59cf39d9e5b305e1c564dee162fb7b549ef3af7
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-10-30 22:03:30 +0300

    added comments and function for formating json

--- api/project/models/project.js
+++ api/project/models/project.js
@@ -8,6 +8,10 @@
 const axios = require('axios');
 const _ = require('lodash');
 
+const formatJSON = (jsonData) => {
+  return `\`\`\`JSON\n${JSON.stringify(jsonData, null, 1)}\n\`\`\``;
+};
+
 module.exports = {
   lifecycles: {
     async beforeUpdate(params, data) {
@@ -15,7 +19,9 @@ module.exports = {
         let projectBeforeModification = await strapi
           .query('project')
           .findOne(params, ['heroImage', 'heroImage.formats']);
-        let concatinated = [
+
+        // We're concatenating the openPositions before and after the modification in one array then we filter out the duplication
+        const concatinated = [
           ...projectBeforeModification.openPositions,
           ...data.openPositions,
         ]
@@ -30,83 +36,67 @@ module.exports = {
               ) === i
           )
           .slice(projectBeforeModification.openPositions.length);
-        let deletedFields = _.differenceBy(
+
+        const deletedFields = _.differenceBy(
           projectBeforeModification.openPositions,
           data.openPositions,
           'id'
         );
 
-       
         if (
           !_.isEqual(
             projectBeforeModification.openPositions,
             data.openPositions
-          ) && process.env.NODE_ENV == 'production'
+          ) &&
+          process.env.NODE_ENV == 'production'
         ) {
-          await axios.post(
-            process.env.OPEN_POSITIONS_DISCORD_WEBHOOK,
-            {
-              username: `${projectBeforeModification.title} Notifier`,
-              avatar_url:
-                'https://avatars.githubusercontent.com/u/53379976?s=200&v=4',
-              embeds: [
-                {
-                  title: `${projectBeforeModification.title} Project Page`,
-                  url: `${process.env.DOMAIN}/projects/${projectBeforeModification.slug}`,
-                  description: `*someone* **__UPDATED__** the open roles => [Link to the strapi page](${process.env.FRONTEND_URL}/admin/plugins/content-manager/collectionType/application::project.project/${params.id})`,
-                  color: 15258703,
-                  fields: [
-                    {
-                      name: 'Before Modification',
-                      value: `\`\`\`JSON\n${JSON.stringify(
-                        projectBeforeModification.openPositions,
-                        null,
-                        1
-                      )}\n\`\`\``,
-                      inline: false,
-                    },
-                    {
-                      name: 'Updated Fields',
-                      value: `\`\`\`JSON\n${JSON.stringify(
-                        concatinated.filter((pos) => pos.id && !pos.isHidden),
-                        null,
-                        1
-                      )}\n\`\`\``,
-                      inline: false,
-                    },
-                    {
-                      name: 'Added Fields',
-                      value: `\`\`\`JSON\n${JSON.stringify(
-                        concatinated.filter((pos) => !pos.id),
-                        null,
-                        1
-                      )}\n\`\`\``,
-                      inline: true,
-                    },
-                    {
-                      name: 'Deleted/Hidden Fields',
-                      value: `\`\`\`JSON\n${JSON.stringify(
-                        [
-                          ...deletedFields,
-                          ...concatinated.filter((pos) => pos.isHidden),
-                        ],
-                        null,
-                        1
-                      )}\n\`\`\``,
-                      inline: true,
-                    },
-                    {
-                      name: 'Thanks!',
-                      value: ':DevLaunchers:',
-                    },
-                  ],
-                  image: {
-                    url: `${projectBeforeModification.heroImage.url}`,
+          await axios.post(process.env.OPEN_POSITIONS_DISCORD_WEBHOOK, {
+            username: `${projectBeforeModification.title} Notifier`,
+            avatar_url:
+              'https://avatars.githubusercontent.com/u/53379976?s=200&v=4',
+            embeds: [
+              {
+                title: `${projectBeforeModification.title} Project Page`,
+                url: `${process.env.DOMAIN}/projects/${projectBeforeModification.slug}`,
+                description: `*someone* **__UPDATED__** the open roles => [Link to the strapi page](${process.env.FRONTEND_URL}/admin/plugins/content-manager/collectionType/application::project.project/${params.id})`,
+                color: 15258703, // A yellow color in "decimal value"
+                fields: [
+                  {
+                    name: 'Before Modification',
+                    value: formatJSON(projectBeforeModification.openPositions),
+                    inline: false,
                   },
+                  {
+                    name: 'Updated Fields',
+                    value: formatJSON(
+                      concatinated.filter((pos) => pos.id && !pos.isHidden)
+                    ),
+                    inline: false,
+                  },
+                  {
+                    name: 'Added Fields',
+                    value: formatJSON(concatinated.filter((pos) => !pos.id)),
+                    inline: true,
+                  },
+                  {
+                    name: 'Deleted/Hidden Fields',
+                    value: formatJSON([
+                      ...deletedFields,
+                      ...concatinated.filter((pos) => pos.isHidden),
+                    ]),
+                    inline: true,
+                  },
+                  {
+                    name: 'Thanks!',
+                    value: ':DevLaunchers:',
+                  },
+                ],
+                image: {
+                  url: `${projectBeforeModification.heroImage.url}`,
                 },
-              ],
-            }
-          );
+              },
+            ],
+          });
         }
       } catch (error) {
         console.error(error);
