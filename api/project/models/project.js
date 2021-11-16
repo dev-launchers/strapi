3ffbea44df4f0dd81b3db9a60b6fa00637609876
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-11-16 07:27:18 +0300

    fix(Enjoy2Live): url of discord

--- api/project/models/project.js
+++ api/project/models/project.js
@@ -57,7 +57,7 @@ module.exports = {
             embeds: [
               {
                 title: `${projectBeforeModification.title} Project Page`,
-                url: `${process.env.DOMAIN}/projects/${projectBeforeModification.slug}`,
+                url: `${process.env.FRONTEND_URL}/projects/${projectBeforeModification.slug}`,
                 description: `*someone* **__UPDATED__** the open roles => [Link to the strapi page](${process.env.FRONTEND_URL}/admin/plugins/content-manager/collectionType/application::project.project/${params.id})`,
                 color: 15258703, // A yellow color in "decimal value"
                 fields: [
