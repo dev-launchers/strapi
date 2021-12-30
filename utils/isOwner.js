5b8ab9c41f6714473a4c15514f79583ecfe725ae
alearm246 <47065061+alearm246@users.noreply.github.com>
2021-12-30 01:16:15 -0600

    fix(alearm246): fix es lint issue

--- utils/isOwner.js
+++ utils/isOwner.js
@@ -1,15 +1,15 @@
 const isOwner = async (collection, userId, id) => {
-    try {
-        const singleCollection = await collection.findOne({ id });
+  try {
+    const singleCollection = await collection.findOne({ id });
 
-        if(userId === singleCollection.user.id){
-            return true;
-        }
-
-        return false;
-    } catch(err) {
-        console.error(err);
+    if(userId === singleCollection.user.id){
+        return true;
     }
-}
+
+    return false;
+  } catch(err) {
+    console.error(err);
+  }
+};
 
 module.exports = isOwner;
