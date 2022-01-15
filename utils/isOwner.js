8d4c1e73afe0bfdcbe39f3c8644507e05dc166f7
alearm246 <47065061+alearm246@users.noreply.github.com>
2022-01-15 01:08:51 -0600

    fix(alearm246): retrieve configmap and kustmization files and add validation logic

--- utils/isOwner.js
+++ utils/isOwner.js
@@ -2,6 +2,10 @@ const isOwner = async (collection, userId, id) => {
   try {
     const singleCollection = await collection.findOne({ id });
 
+    if(!singleCollection) throw new Error('A record with that id was not found');
+
+    if(!singleCollection.user?.id) throw new Error('This collection does not have a user id');
+
     if(userId === singleCollection.user.id){
       return true;
     }
@@ -9,6 +13,7 @@ const isOwner = async (collection, userId, id) => {
     return false;
   } catch(err) {
     console.error(err);
+    return false;
   }
 };
 
