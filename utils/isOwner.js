ddb14a202c82b5efe990996f687506069c572483
alearm246 <47065061+alearm246@users.noreply.github.com>
2022-01-29 13:48:01 -0600

    fix(alearm246): remove catch block since it's redundant

--- utils/isOwner.js
+++ utils/isOwner.js
@@ -1,19 +1,15 @@
 const isOwner = async (collection, userId, id) => {
-  try {
-    const singleCollection = await collection.findOne({ id });
+  const singleCollection = await collection.findOne({ id });
 
-    if(!singleCollection) throw strapi.errors.badRequest('A record with that id was not found');
+  if(!singleCollection) throw strapi.errors.badRequest('A record with that id was not found');
 
-    if(!singleCollection.user?.id) throw strapi.errors.badRequest('This collection does not have a user id');
+  if(!singleCollection.user?.id) throw strapi.errors.badRequest('This collection does not have a user id');
 
-    if(userId === singleCollection.user.id){
-      return true;
-    }
-
-    return false;
-  } catch(err) {
-    throw new Error(err);
+  if(userId === singleCollection.user.id){
+    return true;
   }
+
+  return false;
 };
 
 module.exports = isOwner;
