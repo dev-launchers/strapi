8e9a79b12bd48d8ac24572292173ec4eae2e4a91
Jingwei Wang <wang-jing-wei@hotmail.com>
2023-03-31 18:27:09 -0500

    add firstName and lastName field, and create realted updata lifthook

--- extensions/users-permissions/models/User.js
+++ extensions/users-permissions/models/User.js
@@ -5,9 +5,24 @@ module.exports = {
    * Triggered before user creation.
    */
   lifecycles: {
+    // async beforeUpdate(params, data) {
+    //   if (data.username)
+    //     data.username = data.username.trim().replace(/\s+/g, '_');
+    // },
     async beforeUpdate(params, data) {
-      if (data.username)
+      const {username, firstName, lastName} = data;
+      if (username !== null && username !== undefined) {
+        strapi.log.info('username');
         data.username = data.username.trim().replace(/\s+/g, '_');
+      }
+      if (firstName !== null && firstName !== undefined) {
+        strapi.log.info('firstName');
+        data.firstName = data.firstName.trim().replace(/\s+/g, '_');
+      }
+      if (lastName !== null && lastName !== undefined) {
+        strapi.log.info('lastName');
+        data.lastName = data.lastName.trim().replace(/\s+/g, '_');
+      }
     },
   },
 };
