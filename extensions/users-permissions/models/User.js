e2792de99a2a61cad8ff025b8f7c533cd93699ec
Jingwei Wang <wang-jing-wei@hotmail.com>
2023-04-01 20:40:33 -0500

    change repeated code

--- extensions/users-permissions/models/User.js
+++ extensions/users-permissions/models/User.js
@@ -1,5 +1,11 @@
 'use strict';
 
+function isEmpty(val){
+    return (val === undefined || val == null || val.length <= 0) ? true : false;
+}
+function replaceSpaces(val){
+    return val.trim().replace(/\s+/g, '_');
+}
 module.exports = {
   /**
    * Triggered before user creation.
@@ -11,17 +17,14 @@ module.exports = {
     // },
     async beforeUpdate(params, data) {
       const {username, firstName, lastName} = data;
-      if (username !== null && username !== undefined) {
-        strapi.log.info('username');
-        data.username = data.username.trim().replace(/\s+/g, '_');
+      if (!isEmpty(username)) {
+        data.username = replaceSpaces(username);
       }
-      if (firstName !== null && firstName !== undefined) {
-        strapi.log.info('firstName');
-        data.firstName = data.firstName.trim().replace(/\s+/g, '_');
+      if (!isEmpty(firstName)) {
+        data.firstName = replaceSpaces(firstName);
       }
-      if (lastName !== null && lastName !== undefined) {
-        strapi.log.info('lastName');
-        data.lastName = data.lastName.trim().replace(/\s+/g, '_');
+      if (!isEmpty(lastName)) {
+        data.lastName = replaceSpaces(lastName);
       }
     },
   },
